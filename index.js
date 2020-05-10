780b2c836cd6ebe6243fdd4e5a00d06337d7d6c7
ayush8010720467 <ayushkatiyar096@gmail.com>
2020-05-10 16:07:56 +0530

    Added the colors for whiteboard

--- index.js
+++ index.js
@@ -19,23 +19,43 @@ $('#white_board').mouseup(function (e) {
 $('#white_board').mouseleave(function (e) {
     paint = false;
 });
+$('#choosePurpleSimpleColors').click(()=>{
+    curColor = colorPurple;
+})
+$('#chooseGreenSimpleColors').click(()=>{
+    curColor = colorGreen;
+})
+$('#chooseYellowSimpleColors').click(()=>{
+    curColor = colorYellow;
+})
+$('#chooseBrownSimpleColors').click(()=>{
+    curColor = colorBrown;
+})
 var clickX = new Array();
 var clickY = new Array();
 var clickDrag = new Array();
+var colorPurple = "#cb3594";
+var colorGreen = "#659b41";
+var colorYellow = "#ffcf33";
+var colorBrown = "#986928";
+
+var curColor = colorPurple;
+var clickColor = new Array();
 var paint;
 
 function addClick(x, y, dragging) {
     clickX.push(x);
     clickY.push(y);
     clickDrag.push(dragging);
+    clickColor.push(curColor);
 }
 function redraw() {
     context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
 
-    context.strokeStyle = "#df4b26";
+    // context.strokeStyle = "#df4b26";
     context.lineJoin = "round";
-    context.lineWidth = 5;
-
+    context.lineWidth = $('#size').val(); // change the size of the stroke
+    
     for (var i = 0; i < clickX.length; i++) {
         context.beginPath();
         if (clickDrag[i] && i) {
@@ -45,6 +65,7 @@ function redraw() {
         }
         context.lineTo(clickX[i], clickY[i]);
         context.closePath();
+        context.strokeStyle = clickColor[i]
         context.stroke();
     }
 }
