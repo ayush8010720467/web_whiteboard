652ab8433638088e52e0ff0ac6dcdc3bc215cc6a
iamziike <ziikealaje@outlook.com>
2022-01-28 22:01:10 +0000

    Removed unused variables and replaced var declations with let and const where appropriate. Added spacing to the project

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-window.web_whiteboard = {}
-web_whiteboard = (function(){
-    let context = document.getElementById('white_board').getContext("2d");
-    let wb = document.getElementById('white_board')
+const web_whiteboard = (function(){
+    const COLOR_WHITE = '#ffffff';
+    const COLOR_BLACK = '#000000';
+
+    const wb = document.getElementById('white_board');
+    const context = wb.getContext('2d');
     wb.height = window.innerHeight;
     wb.width = window.innerWidth;
-    let clickX = new Array();
-    let clickY = new Array();
-    let clickDrag = new Array();
-    let colorWhite = "#ffffff";
-    let colorBlack = "#000000";
-    let currentColor = colorBlack;
     
-    let curColor = colorWhite;
-    let clickColor = new Array();
+    let clickX = [];
+    let clickY = [];
+    let clickDrag = [];
+    let currentColor = COLOR_BLACK;
+    
+    let curColor = COLOR_WHITE;
+    const clickColor = [];
     let paint;
 
     const getElement = (selector) => {
@@ -23,7 +24,7 @@ web_whiteboard = (function(){
         getElement(selector).addEventListener(eventType, listener);
     };
 
-    attachEvent("#white_board", "mousedown", (e) => {
+    attachEvent('#white_board', 'mousedown', (e) => {
         const mouseX = e.pageX - wb.offsetLeft;
         const mouseY = e.pageY - wb.offsetTop;
         
@@ -32,23 +33,23 @@ web_whiteboard = (function(){
         redraw();
     });
 
-    attachEvent("#white_board", "mousemove", (e) => {
+    attachEvent('#white_board', 'mousemove', (e) => {
         if (paint) {
             addClick(e.pageX - wb.offsetLeft, e.pageY - wb.offsetTop, true);
             redraw();
         }
     });
 
-    attachEvent("#white_board", "mouseup", () => {
+    attachEvent('#white_board', 'mouseup', () => {
         paint = false;
     });
 
-    attachEvent("#white_board", "mouseleave", () => {
+    attachEvent('#white_board', 'mouseleave', () => {
         paint = false;
     });
 
-    attachEvent("#toggleTheme", "change", () => {
-        toggleColor(getElement("#toggleTheme").checked);
+    attachEvent('#toggleTheme', 'change', () => {
+        toggleColor(getElement('#toggleTheme').checked);
     });
     
     function addClick(x, y, dragging) {
@@ -57,29 +58,28 @@ web_whiteboard = (function(){
         clickDrag.push(dragging);
         clickColor.push(curColor);
     }
-    
-    
+
     function toggleColor(checked) {
         if (checked === false) {
-            curColor = colorBlack;
-            currentColor = colorWhite;
+            curColor = COLOR_BLACK;
+            currentColor = COLOR_WHITE;
         } else {
-            curColor = colorWhite;
-            currentColor = colorBlack;
+            curColor = COLOR_WHITE;
+            currentColor = COLOR_BLACK;
         }
-        var currentLength = clickColor.length;
-        clickColor.fill(curColor, 0, currentLength-1)
+        const currentLength = clickColor.length;
+        clickColor.fill(curColor, 0, currentLength - 1);
         redraw();
     }
     
     function redraw() {
         context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
         context.fillStyle = currentColor;
-        context.fillRect(0, 0, wb.width, wb.height)
-            // context.strokeStyle = "#df4b26";
-        context.lineJoin = "round";
+        context.fillRect(0, 0, wb.width, wb.height);
+        // context.strokeStyle = "#df4b26";
+        context.lineJoin = 'round';
     
-        for (var i = 0; i < clickX.length; i++) {
+        for (let i = 0; i < clickX.length; i++) {
             context.beginPath();
             if (clickDrag[i] && i) {
                 context.moveTo(clickX[i - 1], clickY[i - 1]);
@@ -88,45 +88,43 @@ web_whiteboard = (function(){
             }
             context.lineTo(clickX[i], clickY[i]);
             context.closePath();
-            context.strokeStyle = clickColor[i]
-                // context.lineWidth = radius; // change the size of the stroke
+            context.strokeStyle = clickColor[i];
+            // context.lineWidth = radius; // change the size of the stroke
             context.stroke();
         }
     }
 
-    attachEvent('#clear_complete',"click", () => {
-        let canvas = document.getElementById('white_board')
+    attachEvent('#clear_complete','click', () => {
         clickX = [];
         clickY = [];
         clickDrag = [];
         context.fillStyle = currentColor;
-        context.fillRect(0, 0, wb.width, wb.height)
+        context.fillRect(0, 0, wb.width, wb.height);
     });
     
     function saveImage() {
-        var image = wb.toDataURL()
-        aLink = document.getElementById('test')
-        aLink.download = 'web_whiteboard.png'
+        const image = wb.toDataURL();
+        const aLink = document.getElementById('test');
+        aLink.download = 'web_whiteboard.png';
         aLink.href = image;
-        console.log('done')
-        aLink.click()
+        aLink.click();
     }
     
     function createPage() {
-        var counter = 1;
+        let counter = 1;
         return function() {
             wb.height = window.innerHeight * (++counter);
             window.scrollTo(0, window.innerHeight * (counter - 1));
             redraw();
-        }
+        };
     }
+
     function undo(){
         if(clickX.length>20){
-            clickX = clickX.slice(0, clickX.length-10);
-            clickY = clickY.slice(0, clickY.length-10);
-            clickDrag = clickDrag.slice(0, clickDrag.length-10);
-        }
-        else{
+            clickX = clickX.slice(0, clickX.length - 10);
+            clickY = clickY.slice(0, clickY.length - 10);
+            clickDrag = clickDrag.slice(0, clickDrag.length - 10);
+        } else{
             clickX = [];
             clickY = [];
             clickDrag = [];
@@ -134,12 +132,14 @@ web_whiteboard = (function(){
         redraw();
     }
     
-    let newPage = createPage();
-    return {newPage, saveImage, undo}
-})()
+    const newPage = createPage();
+    return {newPage, saveImage, undo};
+})();
+
 function eventHandlers(e){
     if (e.ctrlKey && e.key === 'z') {
         web_whiteboard.undo();
     }
 }
+
 document.addEventListener('keyup', eventHandlers, false);
